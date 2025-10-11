8cee4e2a7d36e1ad3f1301d5375e469d0067d3b2
Mohammad Malabeh <malaabehmohamed@gmil.com>
2025-10-11 19:23:50 +0300

    add Project model

--- core/models.py
+++ core/models.py
@@ -40,3 +40,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+# -------------------------------
+# Project Model
+# -------------------------------
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name

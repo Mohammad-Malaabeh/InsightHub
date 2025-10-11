215a06db78e4eba3a654cec4750463c816c4d0a8
Mohammad Malabeh <malaabehmohamed@gmil.com>
2025-10-11 19:23:12 +0300

    add custom User model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# -------------------------------
+# Custom User Model
+# -------------------------------
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=20,
+        choices=[
+            ('Admin', 'Admin'),
+            ('Manager', 'Manager'),
+            ('Staff', 'Staff'),
+        ],
+        default='Staff'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def save(self, *args, **kwargs):
+        self.is_superuser = bool(self.is_superuser)
+        self.is_staff = bool(self.is_staff)
+
+        if self.is_superuser:
+            if self.role != 'Admin':
+                self.role = 'Admin'
+            self.is_staff = True
+        else:
+            if self.role == 'Admin':
+                self.is_superuser = True
+                self.is_staff = True
+            else:
+                pass
+
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.username

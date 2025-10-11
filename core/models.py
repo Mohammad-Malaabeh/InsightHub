a79e565df291ddeca4826a243973d73cc6638248
Mohammad Malabeh <malaabehmohamed@gmil.com>
2025-10-11 19:27:05 +0300

    add Post  model

--- core/models.py
+++ core/models.py
@@ -77,3 +77,19 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# -------------------------------
+# Post Model
+# -------------------------------
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+    liked_by = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return self.title
